f82fbfdb67e405eb7b07423be45d0c4704ab2361
Yegor Trussov <egortr2004@mail.ru>
2020-10-04 17:54:26 +0600

    project overlay elements apper anumation added

--- portfolio/src/components/Projects/ShowProjectOverlay/ShowProjectOverlay.js
+++ portfolio/src/components/Projects/ShowProjectOverlay/ShowProjectOverlay.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight, faArrowUp, faEye, faTimes } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +10,39 @@ export default class ShowProjectOverlay extends Component {
     state = {
         isShown: false
     }
+
+    constructor(props) {
+        super(props);
+
+        this.mainBlockEl = createRef();
+        this.imageEl = createRef();
+        this.descriptionEl = createRef();
+    }
+
+    componentDidMount() {
+        let mainBlockEl = this.mainBlockEl.current;
+        let imageEl = this.imageEl.current;
+        let descriptionEl = this.descriptionEl.current;
+        console.log(descriptionEl)
+
+        mainBlockEl.classList.add('animated', 'show');
+        // mainBlockEl.classList.add('animated');
+        // imageEl.classList.add('animated', 'show');
+        // descriptionEl.classList.add('animated', 'show');
+
+        window.setTimeout(() => {
+            // mainBlockEl.classList.remove('animated', 'show')
+            imageEl.classList.add('animated', 'show');
+            descriptionEl.classList.add('animated', 'show');
+
+            // window.setTimeout(() => {
+            //     imageEl.classList.remove('animated', 'show');
+            //     imageEl.classList.add('displayed');
+            //     descriptionEl.classList.remove('animated', 'show');
+            // }, 1000)
+        }, 500)
+    }
+    
 
     toggleInfoBlock() {
         let { isShown } = this.state;
@@ -27,9 +60,9 @@ export default class ShowProjectOverlay extends Component {
         const { project, projectsNum } = this.props;
 
         return (
-            <div className="show-project-overlay">
+            <div ref={ this.mainBlockEl } className="show-project-overlay">
                 <div className="container">
-                    <div className="image">
+                    <div ref={ this.imageEl } className="image">
                         <img src="https://cdn.jpegmini.com/user/images/slider_puffin_jpegmini_mobile.jpg" alt=""/>
                         <div className="move-btns">
                             <div className="move-btn">
@@ -51,7 +84,7 @@ export default class ShowProjectOverlay extends Component {
                         </button>
                     </div> 
                     <div className={ `info ${ isShown ? 'shown' : 'hidden' }` }>
-                        <div className="description" onClick={ () => this.toggleInfoBlock() }>
+                        <div ref={ this.descriptionEl } className="description" onClick={ () => this.toggleInfoBlock() }>
                             <button className={ `toggle-block ${ isShown ? 'shown' : 'hidden' }` }>
                                 <FontAwesomeIcon 
                                     className={ `icon ${ isShown ? 'shown' : 'hidden' }`}
