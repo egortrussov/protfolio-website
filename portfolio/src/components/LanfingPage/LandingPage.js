701c8b70dbaafe649b1918470634398e9e643733
Yegor Trussov <egortr2004@mail.ru>
2020-10-16 18:25:55 +0600

    heroku predeploy, mobile scroll bug fixed

--- portfolio/src/components/LanfingPage/LandingPage.js
+++ portfolio/src/components/LanfingPage/LandingPage.js
@@ -71,6 +71,59 @@ export default class LandingPage extends Component {
             })
             
         })
+        window.addEventListener('scroll', (e) => {
+            let { isAnimated, currentPage, pageNum, extraClasses } = this.state;
+
+            if (isAnimated) 
+                return false;
+            
+            let { deltaY } = e;
+            if (deltaY < 0 && currentPage == 0 || deltaY > 0 && currentPage == pageNum - 1 || deltaY == 0) {
+                // console.log('object')
+                return false;
+            }
+            // if (deltaY < 0) console.log('Nooo')
+            // console.log('yey')
+            console.log(e)
+            
+            let animProperties = {}
+            if (deltaY > 0) {
+                // console.log('DOWNNNN')
+                animProperties = {
+                    hide: currentPage,
+                    show: currentPage + 1,
+                    dir: 1
+                }
+                extraClasses[currentPage] = 'hidden up';
+                extraClasses[currentPage + 1] = 'shown';
+                currentPage++;
+            } else {
+                animProperties = {
+                    hide: currentPage,
+                    show: currentPage - 1,
+                    dir: -1
+                }
+                extraClasses[currentPage] = 'hidden down';
+                extraClasses[currentPage - 1] = 'shown';
+                currentPage--;
+            } 
+
+            window.setTimeout(() => {
+                this.setState({
+                    isAnimated: false
+                })
+            }, 1500)
+
+            console.log(extraClasses)
+
+            this.setState({
+                isAnimated: true,
+                currentPage,
+                animProperties,
+                extraClasses
+            })
+            
+        })
     }
     
 
