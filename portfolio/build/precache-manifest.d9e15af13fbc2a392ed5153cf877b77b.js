0f724621252810442d8f43c6afffefe136703968
Yegor Trussov <egortr2004@mail.ru>
2020-10-16 18:13:52 +0600

    heroku predeploy, simple server created

---
+++ portfolio/build/precache-manifest.d9e15af13fbc2a392ed5153cf877b77b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9f81214e07f3294561aeae4c89ec86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "192bd2d954d85dfabe05",
+    "url": "/static/css/main.b2c3b9ec.chunk.css"
+  },
+  {
+    "revision": "802cbb5a2983627c4cda",
+    "url": "/static/js/2.47c6d9b2.chunk.js"
+  },
+  {
+    "revision": "554bb6d7f0cceb0c5df083690406398b",
+    "url": "/static/js/2.47c6d9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192bd2d954d85dfabe05",
+    "url": "/static/js/main.a42ada2e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1f768a6a76de8db65c374c4749281ecf",
+    "url": "/static/media/contact-image.1f768a6a.svg"
+  },
+  {
+    "revision": "231bd9d7357373a07418ad01b0b05d6f",
+    "url": "/static/media/layer1.231bd9d7.png"
+  },
+  {
+    "revision": "69bded30f9edbd4875464e3c40b2988d",
+    "url": "/static/media/layer2.69bded30.png"
+  },
+  {
+    "revision": "09b32d78c7052b9d307a845217469d82",
+    "url": "/static/media/layer3.09b32d78.png"
+  },
+  {
+    "revision": "67d08c1c454fee39ee14fb38a13845e1",
+    "url": "/static/media/layer4.67d08c1c.png"
+  },
+  {
+    "revision": "30355a93c4bfdf9fdce8c21e55ed524f",
+    "url": "/static/media/layer5.30355a93.png"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/static/media/nodejs.521444a2.png"
+  },
+  {
+    "revision": "4826dbbe4813ee33bb9dba578d1731c5",
+    "url": "/static/media/project1.4826dbbe.jpg"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/react.638be909.png"
+  }
+]);
